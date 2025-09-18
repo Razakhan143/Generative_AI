ed552748cac25adb6df7e7539e615ac230bbc976
Raza Khan <razakhanzada100@gmai.com>
2025-09-19 00:04:14 +0500

    add some changes on both ends

--- Gen-AI-Projects/AI-Resume-Job-Insight/Frontend/app/firebaseConfig.js
+++ Gen-AI-Projects/AI-Resume-Job-Insight/Frontend/app/firebaseConfig.js
@@ -6,16 +6,15 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCtD77j8itJQUHvMtKg_HAnhgz6wO7khmw",
-  authDomain: "ai-resume-job-insight.firebaseapp.com",
-  projectId: "ai-resume-job-insight",
-  storageBucket: "ai-resume-job-insight.firebasestorage.app",
-  messagingSenderId: "676401966782",
-  appId: "1:676401966782:web:e39ab509eac150b10878ef",
-  measurementId: "G-L5HYDW2R45"
-};
 
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
